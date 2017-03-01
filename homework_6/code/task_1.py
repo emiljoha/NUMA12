89c6659e5e2725f1f13158403edb4a987504fa57
Emil Johansson <emil.sweden@gmail.com>
2017-03-01 16:37:51 +0100

    Task 1 complete.

--- homework_6/code/task_1.py
+++ homework_6/code/task_1.py
@@ -8,13 +8,14 @@ Q2 = a + b*x + x**2
 
 # scalar product
 def scalar(func1, func2):
+    #print(sp.latex(sp.integrate(x*func1*func2, (x, 0, 1))))
     return sp.integrate(x*func1*func2, (x, 0, 1))
 
 # Need to make Q2 such that it is orthogonal to all elements in
 # P_1.
 sol = sp.solve([scalar(Q2, 1), scalar(Q2, x)]) 
 Q2 = Q2.subs([(a, sol[a]), (b, sol[b])])
-
+#print(sp.latex(sol[a]) + "     " + sp.latex(sol[b]))
 # Roots to Q2 will now be out X_i´s 
 x0, x1 = sp.solve(Q2, x)
 
@@ -36,16 +37,16 @@ l0, l1 = lagrange([x0, x1])
 w0 = sp.simplify(scalar(l0, 1))
 w1 = sp.simplify(scalar(l1, 1));  
 
-print('x_0 = ' + sp.latex(x0))
-print('x_1 = ' + sp.latex(x1))
-print('w_0 = ' + sp.latex(w0))
-print('w_1 = ' + sp.latex(w1))
+# print('x_0 = ' + sp.latex(x0))
+# print('x_1 = ' + sp.latex(x1))
+# print('w_0 = ' + sp.latex(w0))
+# print('w_1 = ' + sp.latex(w1))
 
 
 #sys.stdout.write
 
 f = a + b*x + c*x**2
-approx = sp.simplify(w0*f.subs(x, x0) + w1*f.subs(x, x1))
+approx = w0*f.subs(x, x0) + w1*f.subs(x, x1)
 print(sp.latex(approx))
 #print(sp.integrate(x*f, (x, 0, 1)))
 print(sp.simplify(approx - sp.integrate(x*f, (x, 0, 1))) == 0)
