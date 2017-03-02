277b928f40a4d8b4e1b6e51a47d3fe10f8a3f15c
Emil Johansson <emil.sweden@gmail.com>
2017-03-02 16:09:05 +0100

    Heureka task 1 on homework 5 is finally working
    Removed all the symbolics and worked out the bugs and now it works.

--- homework_5/code/task_1.py
+++ homework_5/code/task_1.py
@@ -2,123 +2,131 @@
 
 from sympy import symbols, lambdify, sin
 import numpy as np
+from matplotlib import pyplot as plt
 
 def sign(x):
     if x <= 0:
         return -1
     if x > 0:
         return 1
-
-# @param f Continuos function to be approximated
-# @param n Dimension of appriximation space
-# @param intervall [a, b] end points of closed intervall on whitch the
-#                  function f is to be approximated.
-# @param refrence Ordered list of n+1 distingt point in [a, b]
-# @param tolerance When h changes less that tolerance the algorithm ends.
-# @param nsp Number of sample points when calculating maxima of error function.
-# @param basis Optional set of n Sympy expression that form the functional
-#              basis for the approximation set. If not provided the approximation
-#              set of polynomials of degree n-1 with the monomial basis is used.
-
-# @return Coefficients Array of coefficients for the found approximation in the basis.
-# @return Error Estimate of the error (distance to the best approximation
-#               in the max-norm
-# @return h_history Vector with the reference level values h for every iteration.
-# @return final_reference The current reference when done. 
-def exchange_algorithm (f_symb, n, intervall, reference, tolerance, nsp, basis_symb=" " ):
-    if basis_symb == " ":
-        basis_symb = []
-        x = symbols('x')
-        for i in range(n):
-            basis_symb.append(x**i)
-            
-    basis = []
-    for base_s in basis_symb:
-        basis.append(lambdify(x, base_s, "numpy"))
-        
-    f = lambdify(x, f_symb, "numpy")
-
-    a, b = intervall
-        
-    def hcoeff(vec):
+     
+def hcoeff(x):
+    try:
         res = [];
-        for k in vec:
+        for k in x:
             res.append((-1)**k);
-        return res; 
-
-    def approx(coef, vec):
-        res = np.zeros(len(vec));
-        for i in range(len(coef)):
-            for j in range(len(vec)): 
-                res[j] += coef[i] * basis[i](vec[j]);
         return res;
+    except TypeError:
+        return (-1)**k;
+
+
+def polynom(x, basis, coef):
+    try:
+        res = np.zeros(len(x));
+        for i in range(len(coef)):
+            for j in range(len(x)): 
+                res[j] += coef[i] * basis(x[j], i);
+        return res;
+    except TypeError: # x not list but number.
+        res = 0;
+        for i in range(len(coef)):
+                res[j] += coef[i] * basis(x, i);
+        return res;
+
+def error(f, x, basis, coef):
+    return f(x) - polynom(x, basis, coef);
+
+
+def exchange_algorithm (f, n, intervall, reference, tolerance, nsp, basis=pow):
+    if (len(reference) != n + 1):
+        raise ValueError("Reference must have exactly n+1 elements")
     
+    a, b = intervall
     
     matrix = np.zeros([n+1, n+1])
     grid = np.linspace(a, b, num=nsp)
     h = [];
-    count = 0; 
+    count = 0;
 
+    legend = []; # for plotting
+    
     while ( (len(h) < 2
              or (abs(h[len(h)-1] - h[len(h) - 2]) > tolerance))
-            and count < 100):
+            and count < 10):
         count += 1
         # Set up matrix from reference 
         for i in range(n):
-            matrix[i] = basis[i](reference)
+            for j in range(n+1):
+                matrix[i][j]= basis(reference[j], i)
         matrix[n] = hcoeff(np.array(range(n+1)))
-
+        
         #calculate coefficients and h
         #res =  np.linalg.inv(np.transpose(matrix)).dot(f(reference))
         res = np.linalg.solve(np.transpose(matrix), f(reference))
         h.append(res[n])
-
-
+        
         # Find the max
-        error_grid = f(grid) - approx(np.delete(res, -1), grid);
-        max_index = np.argmax(error_grid); 
+        coefficients = np.delete(res, -1);
+        error_grid = error(f, grid, basis, coefficients);
+        error_ref = error(f, reference, basis, coefficients);
+        max_index = np.argmax(np.abs(error_grid)); 
         maxpos = grid[max_index]; 
         max_error = error_grid[max_index];
-        
-        print(max_error); 
-        
-        # Test if we are done. 
-        for i in range(n):
-            if (maxpos == reference[i]):
-                return res, h, maxpos;
+        plt.plot(grid, error_grid);
+        #plt.plot(reference, np.zeros(len(reference)), "*")
+        legend.append(str(count))
 
         #Case: Before first
         if (maxpos < reference[0]):
-            if (sign(max_error) == sign(error(reference[0])) ):
+            if (sign(max_error) == sign(error_ref[0])):
                 reference[0] = maxpos
             else:
                 reference = np.delete(reference, n)#delete last element
                 reference = np.insert(reference, 0, maxpos) #insert maxpos as first
+        
         #Case: between two reference points
-        for i in range(len(reference)-2):
+        for i in range(len(reference)-1):
             if ( (maxpos > reference[i]) and (maxpos < reference[i+1]) ):
-                if (sign(max_error) == sign(error(reference[i]))):
+                if (sign(max_error) == sign(error_ref[i])):
                     reference[i] = maxpos
                 else:
-                    reference[i+1] = maxpos                
+                    reference[i+1] = maxpos
+ 
         #Case: After last point
         if (maxpos > reference[n]):
-            if (sign(max_error) == sign(error(reference[n]))):
+            if (sign(max_error) == sign(error_ref[n])):
                 reference[n] = maxpos
             else:
                 reference = np.delete(reference, 0) #delete first element.
                 reference = np.append(reference, maxpos) # append with new last element.
-        
-    return res, h, error(maxpos), reference
-
+    plt.legend(legend)
+    plt.savefig("error.png")
+    plt.figure(2);
+    plt.plot(grid, f(grid), grid, polynom(grid, basis, coefficients))
+    plt.legend(["f", "approx"])
+    plt.savefig("approx.png")
+    return coefficients, np.abs(max_error), count, h, reference; 
     
 if __name__ == "__main__":
+    def f(x):
+        try:
+            return 1 / (1 + 25*x**2);
+        except TypeError:
+            res = [];
+            for y in x:
+                res.append(1 / (1 + 25*x**2));
+            return res; 
+    
     x = symbols('x')
-    n = 2
+    n = 40
     intervall = (0, 1)
-    reference = np.array([0.1, 0.5, 1])
-    f = x**2 #(1 + 25*x**2)**(-1)
-    tolerance = 0.01
-    nsp = 1000
-    res, h, maxpos= exchange_algorithm(f, n, intervall, reference, tolerance, nsp)
-    print(h)
+    reference = np.linspace(0, 1, n+1)
+    tolerance = 10**(-10)
+    nsp = 100
+    coef, error, count, h, reference = exchange_algorithm(f, n, intervall, reference, tolerance, nsp)
+    print("Coefficients: " + str(coef))
+    print("error: " + str(error))
+    print("count: " + str(count))
+    print("h: " + str(h))
+    print("reference: " + str(reference))
+    
